27fca00331c5d98ddd45e533b6cde5d16a06c26b
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-06-27 16:39:47 +0900

    firstComit

---
+++ CustomTransitions2/CustomAnimator.swift
@@ -0,0 +1,9 @@
+//
+//  CustomAnimator.swift
+//  CustomTransitions2
+//
+//  Created by 福山帆士 on 2020/06/27.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import Foundation
---
+++ CustomTransitions2/CustomPresentationController.swift
@@ -0,0 +1,13 @@
+//
+//  CustomPresentationController.swift
+//  CustomTransitions2
+//
+//  Created by 福山帆士 on 2020/06/27.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import UIKit
+
+class CustomPresentationController: UIPresentationController {
+
+}
